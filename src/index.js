22631ac003a23f3a9424cd7e080af73081fb49c3
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-18 03:58:29 +0200

    empty input fix

--- src/index.js
+++ src/index.js
@@ -6,24 +6,23 @@ import countryInfoMarkUp from './templates/countryInfoMarkUp.hbs';
 import countriesListMarkUp from './templates/countriesListMarkUp.hbs';
 
 const DEBOUNCE_DELAY = 300;
-
 const refs = {
     input: document.querySelector('input#search-box'),
-    countryList: document.querySelector('.country-list'),
+    countriesList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
 function inputHandler(e) {
-    const inputValue = e.target.value.toLowerCase().trim();
+    const inputValue = e.target.value.trim();
+    if (!inputValue) {
+        return;
+    }
+    refs.countriesList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
     fetchCountries(inputValue)
         .then(countries => {
-            refs.countryList.innerHTML = '';
-            refs.countryInfo.innerHTML = '';
-            if (!inputValue) {
-                throw new Error('Input is empty');
-            }
             if (countries.length > 10) {
                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
             }
@@ -41,21 +40,15 @@ function inputHandler(e) {
             }
         })
         .catch((error) => {
-            if (error) {
-                console.log(error);
-            } else {
-                Notiflix.Notify.failure("Oops, there is no country with that name");
-            }
+            console.log(error);
+            Notiflix.Notify.failure("Oops, there is no country with that name");
         });
-
 }
-
 
 function createCountriesList(countries) {
     const markUp = countries.map(countriesListMarkUp);
-    refs.countryList.innerHTML = markUp.join('');
+    refs.countriesList.innerHTML = markUp.join('');
 };
-
 
 function createCountryInfo(countries) {
     const markUp = countries.map(countryInfoMarkUp);
